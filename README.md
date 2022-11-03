06ab1bff2c4a821bfe147619aafd8d5f218b53a4
dbuscombe-usgs <dbuscombe@gmail.com>
2022-11-03 16:57:12 -0700

    update readme, update naip-dl codes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ An idealized Doodleverse workflow could be thought of as :
 
 As Earth surface scientists, a key objective for us is to push geospatial imagery through this workflow. Geospatial imagery is often in formats that are not innately compatible with tensorflow (i.e., GeoTIFF). Images can also be large (i.e., many pixels in the X and Y). As a result imagery often needs to be processed to be compatible with the Doodleverse. There are main processing steps which we can think of, and they occur at both ends of the doodler pipeline:
 1. Pre-Doodler: large GeoTIFFs need to be tiled (and maybe even resized), and then converted to jpg imagery.
-2. Pre-Doodler: programmatically access GeoTIFFs for doodling
+2. Pre-Doodler: programmatically access GeoTIFFs for doodling using geemap
 3. Post-Gym: take segmentations and reconstituted a new geotiff with the same size/scale/format of of the original images.
 
 This is where s2m_engine comes into the picture! 
@@ -22,3 +22,18 @@ Tentative roadmap:
 - Others to be detailed at a later date
 ...
 
+This repo is intended to eventually be a pip installable package, hosted on pypi.org. This follows an established practice in the Doodleverse; individual applications such as Gym and Doodler outsource their core functionality to a pip-installable package for two reasons:
+
+1. several downstream applications can use the same core functionality. This is already in evidence with [DashDoodler's](https://github.com/Doodleverse/dash_doodler) and [HoloDoodler](https://github.com/Doodleverse/holodoodler) usage of the [doodler_engine](https://github.com/Doodleverse/doodler_engine) codes, and Gym's and Zoo's common usage of the [doodleverse-utils](https://github.com/Doodleverse/doodleverse_utils) codes. Similarly, this `s2m_engine` repo is designed to work with the (future) [Seg2Map](https://github.com/Doodleverse/seg2map) and [MorphoSedimentaryMapper](https://github.com/dbuscombe-usgs/MorphoSedimentaryMapper) applications
+2. these pip repositories have no specified dependencies; they are intended to be installed into an existing environment specific to the intended application. That could be a conda environment, python virtual environment, or something similar where `pip` packages can be installed
+
+However, for interim testing and development purposes, for conda users, the following conda workflow could be temporarily adopted. These are geospatial workflows so `geopandas` and `shapely` are generally useful for data wrangling like format conversions and reprojecting. `earthengine-api` and `geemap` are suggested for easily accessing geospatial imagery. This also installs `gdal` from OSGeo. This is a crucial dependency, but is hidden in the conda recipe below. `notebook` provides access to IDEs `jupyter` and `ipython` (but vscode, spyder, etc, etc, could alternatively be used)
+
+```
+conda create -n s2m_dev python=3.8
+conda activate s2m_dev
+conda install -c conda-forge geopandas earthengine-api scikit-image matplotlib notebook geemap shapely -y
+pip install area
+```
+
+Use of `ee` and `geemap` require installation of the [gcloud cli](https://cloud.google.com/sdk/docs/install#deb). Then you must run `earthengine authenticate` and sign in with your google account. This is free and requires only a gmail address
--- s2m_engine/NAIP/example_dl_naip_ts_from_geojson.py
+++ s2m_engine/NAIP/example_dl_naip_ts_from_geojson.py
@@ -26,140 +26,181 @@
 # For a list of polygons in a geojson file
 
 # 1. download all the 0.5-m NAIP imagery. Bands downloaded separately and as a multiband geotiff
-# 1a. If the imagery is larger than an allowable GEE data packet (33MB), region is quartered and each quarter
+# 1a. If the imagery is larger than an allowable GEE data packet (33MB), region is split into nx x ny regions
 
-import geemap,ee #, reverse_geocoder
+# (you need to run earthengine authenticate in the cmd first)
+
+import geemap,ee 
 import os, json , shutil
-from ipyleaflet import GeoJSON
 import numpy as np
 from glob import glob
 from area import area
-from joblib import Parallel, delayed
-
 from shapely.geometry import LineString, MultiPolygon, Polygon
 from shapely.ops import split
+from osgeo import gdal
 
-
-
-##  ogr2ogr -t_srs EPSG:4326 -f GEOJSON elwhaMR_epsg4326.geojson elwhaMR.geojson
-
-with open('example.geojson') as f:
-    json_data = json.load(f)
-
-features = json_data['features'][0]
+###=================================================
+############### USER INPUTS #########################
+###=================================================
+## todo: make a config file containing these settings, and read in config file
 
 MAXCLOUD = 5  #percentage maximum cloud cover tolerated
 
+OUT_RES_M = 0.5 #output raster spatial footprint in metres
+
+nx, ny = 4,4 # number of columns and rows to split
+
+site = 'beaches' #name of the site 
+
+#name of file containing 
+roifile = 'example_singleROI.geojson'
+
+## years of data collection (list of strings)
+years = ['2006', '2009','2011','2013','2015','2017']
+
 ###=================================================
-############### NAIP #########################
+############### DOWNLOAD NAIP TILES USING GEEMAP ###
 ###=================================================
+
+# initialize Earth Engine
+# ee.Authenticate()
+ee.Initialize()
+
+## todo: add checks to ensure geojson is valid
+with open(roifile) as f:
+    json_data = json.load(f)
+
+features = json_data['features']#[0]
+
 gee_collection = 'USDA/NAIP/DOQQ'
 
-OUT_RES_M = 0.5 #1
+# cycle through each feature and each year
+# make polygon splits
+# for each split, download the tiles
+# at the end of the feature/year, copy multiband data over to a separate folder
+# then create a VRT, geoTIFF, and JPEG with WLD file
+for featurenumber, feature in enumerate(features):
 
-nx, ny = 4,4 # number of columns and rows
-site = 'elwhaMR'
+    for year in years:
+        print("Working on {}".format(year))
 
+        start_date = year+'-01-01'
+        end_date = year+'-12-31'
 
-for year in ['2006', '2009','2011','2013','2015','2017']:
-
-    start_date = year+'-01-01'
-    end_date = year+'-12-31'
-
-    try:
-        os.mkdir('naip'+year)
-    except:
-        pass
-
-    try:
-        os.mkdir('naip'+year+os.sep+site)
-    except:
-        pass
-
-    # initialize Earth Engine
-    # ee.Authenticate()
-    ee.Initialize()
-
-    coordinates = features['geometry']['coordinates'][0]
-    lng, lat = np.mean(coordinates[0], axis=0)
-    print((lng, lat))
-    area_sqkm = area(features['geometry'])/1e6
-    #print(area_sqkm)
-
-    collection = ee.ImageCollection(gee_collection)
-
-    polygon = Polygon([tuple(c) for c in coordinates[0]])
-    minx, miny, maxx, maxy = polygon.bounds
-
-    dx = (maxx - minx) / nx  # width of a small part
-    dy = (maxy - miny) / ny  # height of a small part
-    horizontal_splitters = [LineString([(minx, miny + i*dy), (maxx, miny + i*dy)]) for i in range(ny)]
-    vertical_splitters = [LineString([(minx + i*dx, miny), (minx + i*dx, maxy)]) for i in range(nx)]
-    splitters = horizontal_splitters + vertical_splitters
-
-    result = polygon
-    for splitter in splitters:
-        result = MultiPolygon(split(result, splitter))
-    parts = [list(part.exterior.coords) for part in result.geoms]
-
-    print("Number of individual tiles: {}".format(len(parts)))
-
-    counter = 1
-    for part in parts:
-        print(counter)
         try:
-            os.mkdir('naip'+year+os.sep+str(site)+os.sep+'chunk'+str(counter))
+            os.mkdir(site)
         except:
             pass
 
-        print(area({'type':'Polygon','coordinates': [[list(p) for p in part]]})/1e6)
+
+        try:
+            os.mkdir(site+os.sep+'feature'+str(featurenumber))
+        except:
+            pass
+
+        try:
+            os.mkdir(site+os.sep+'feature'+str(featurenumber)+os.sep+year)
+        except:
+            pass
+
+        coordinates = feature['geometry']['coordinates'][0]
+
+        lng, lat = np.mean(coordinates, axis=0)
+
+        area_sqkm = area(feature['geometry'])/1e6
+
         collection = ee.ImageCollection(gee_collection)
 
-        polys = ee.Geometry.Polygon(part)
+        polygon = Polygon([tuple(c) for c in coordinates])
 
-        centroid = polys.centroid()
-        lng, lat = centroid.getInfo()['coordinates']
-        #print("lng = {}, lat = {}".format(lng, lat))
+        minx, miny, maxx, maxy = polygon.bounds
 
-        #lng_lat = ee.Geometry.Point(lng, lat)
-        collection = collection.filterBounds(polys)
-        collection = collection.filterDate(start_date, end_date).sort('system:time_start', True)
-        count = collection.size().getInfo()
-        #print("Number of cloudy scenes: ", count)
-        img_lst = collection.toList(1000)
+        dx = (maxx - minx) / nx  # width of a small part
+        dy = (maxy - miny) / ny  # height of a small part
+        horizontal_splitters = [LineString([(minx, miny + i*dy), (maxx, miny + i*dy)]) for i in range(ny)]
+        vertical_splitters = [LineString([(minx + i*dx, miny), (minx + i*dx, maxy)]) for i in range(nx)]
+        splitters = horizontal_splitters + vertical_splitters
 
-        N = []
-        for i in range(0, count):
-            image = ee.Image(img_lst.get(i))
-            name = image.get('system:index').getInfo()
-            N.append(name)
-            print(name)
+        result = polygon
+        for splitter in splitters:
+            result = MultiPolygon(split(result, splitter))
+        parts = [list(part.exterior.coords) for part in result.geoms]
 
-        for n in N:
-            image = ee.Image('USDA/NAIP/DOQQ/'+n)
-            #geemap.ee_export_image(image, os.getcwd()+os.sep+'site_'+str(site)+'_'+n+'_part_'+str(counter)+'_of_'+str(len(parts))+'.tif', scale=OUT_RES_M, region=polys, file_per_band=True)
-            geemap.ee_export_image(image, os.getcwd()+os.sep+'naip'+year+os.sep+str(site)+os.sep+'chunk'+str(counter)+os.sep+'chunk'+str(counter)+'_'+n+'.tif', scale=OUT_RES_M, region=polys, file_per_band=True, crs="EPSG:4326")
-            geemap.ee_export_image(image, os.getcwd()+os.sep+'naip'+year+os.sep+str(site)+os.sep+'chunk'+str(counter)+os.sep+'chunk'+str(counter)+'_'+n+'_multiband.tif', scale=OUT_RES_M, region=polys, file_per_band=False, crs="EPSG:4326")
+        print("Number of individual tiles: {}".format(len(parts)))
 
-        counter += 1
+        counter = 1
+        for part in parts:
+            print("Working on part {} of {}".format(counter, len(parts)))
+            try:
+                os.mkdir(site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'chunk'+str(counter))
+            except:
+                pass
+
+            collection = ee.ImageCollection(gee_collection)
+
+            polys = ee.Geometry.Polygon(part)
+
+            centroid = polys.centroid()
+            lng, lat = centroid.getInfo()['coordinates']
+
+            collection = collection.filterBounds(polys)
+            collection = collection.filterDate(start_date, end_date).sort('system:time_start', True)
+            count = collection.size().getInfo()
+            #print("Number of cloudy scenes: ", count)
+            img_lst = collection.toList(1000)
+
+            N = []
+            for i in range(0, count):
+                image = ee.Image(img_lst.get(i))
+                name = image.get('system:index').getInfo()
+                N.append(name)
+                print(name)
+
+            for n in N:
+                image = ee.Image('USDA/NAIP/DOQQ/'+n)
+                geemap.ee_export_image(image, 
+                                os.getcwd()+os.sep+site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'chunk'+str(counter)+os.sep+'chunk'+str(counter)+'_'+n+'.tif', 
+                                scale=OUT_RES_M, region=polys, file_per_band=True, crs="EPSG:4326")
+
+                geemap.ee_export_image(image, 
+                                os.getcwd()+os.sep+site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'chunk'+str(counter)+os.sep+'chunk'+str(counter)+'_'+n+'_multiband.tif', 
+                                scale=OUT_RES_M, region=polys, file_per_band=False, crs="EPSG:4326")
+
+            counter += 1
 
 
-    # move multiband files into own directory
+        # move multiband files into own directory
 
-    outdirec = os.getcwd()+os.sep+'naip'+year+os.sep+str(site)+os.sep+'multiband'
-    try:
-        os.mkdir( outdirec)
-    except:
-        pass
+        outdirec = os.getcwd()+os.sep+site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'multiband'
+        try:
+            os.mkdir( outdirec)
+        except:
+            pass
 
-    for folder in glob( os.getcwd()+os.sep+'naip'+year+os.sep+site+os.sep+'chunk*', recursive=True ):
-        # print(folder)
-        files = glob(folder+os.sep+'*multiband.tif')
-        # print(files)
-        [shutil.copyfile(file,outdirec+os.sep+file.split(os.sep)[-1]) for file in files]
+        for folder in glob( os.getcwd()+os.sep+site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'chunk*', recursive=True ):
+            files = glob(folder+os.sep+'*multiband.tif')
+            [shutil.copyfile(file,outdirec+os.sep+file.split(os.sep)[-1]) for file in files]
 
 
-    # ## run gdal to merge into big tiff 
-    # os.system('gdalbuildvrt -srcnodata 0 -vrtnodata 0 tmp.vrt '+outdirec+os.sep+'*.tif')
-    # os.system('gdal_translate tmp.vrt mosaic.tif -co BIGTIFF=YES  -co COMPRESS=DEFLATE -co TILED=YES')
+        # ## run gdal to merge into big tiff 
+        try:
+            ## create vrt
+            vrtoptions = gdal.BuildVRTOptions(resampleAlg='average', srcNodata=0, VRTNodata=0)
+            files = glob(outdirec+os.sep+'*multiband.tif')
+            outfile = os.getcwd()+os.sep+site+os.sep+'feature'+str(featurenumber)+os.sep+year+os.sep+'merged_multispectral.vrt'
+            ds = gdal.BuildVRT(outfile, files, options=vrtoptions)
+            ds.FlushCache()
+            ds = None
 
+            # create geotiff
+            ds = gdal.Translate(outfile.replace('.vrt','.tif'), creationOptions=["COMPRESS=LZW", "TILED=YES"], srcDS=outfile)
+            ds.FlushCache()
+            ds = None   
+
+            # create jpeg
+            ds = gdal.Translate(outfile.replace('.vrt','.jpg'), creationOptions=["WORLDFILE=YES", "QUALITY=100"], srcDS=outfile.replace('.vrt','.tif'))
+            ds.FlushCache()
+            ds = None   
+
+        except:
+            print("Data not available")
